02f922255b5ca9b29feb42185fda7d977c9d03e8
ofir <ofirne2000@gmail.com>
2025-05-04 00:21:30 +0300

    fixed bugs

--- firebase_funcs.py
+++ firebase_funcs.py
@@ -1,15 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, db
-import os
-import json
 
-# Get the absolute path to key.json
-current_dir = os.path.dirname(os.path.abspath(__file__))
-key_path = os.path.join(current_dir, 'key.json')
-
-# Load the key.json file
-with open(key_path, 'r') as f:
-    key_data = json.load(f)
 
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate(key_data)
